e25b3234bcd1d3cd85ddd866d879ff0b68456d35
Collins Muema <115388842+CMuema@users.noreply.github.com>
2023-09-28 12:01:49 +0300

    Initial commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,10 @@
+//This is my Hello World program in C++
+/*This C++ file was created by Collins Muema DBIT EC Sep-Dec23*/
+//PreProcessor Directives
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<< "Hello world. This is my first C++ code.";
+}
---
+++ iostreamTest.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string number;
+    cout<< "Enter a number";
+    cin>> number;
+    cout<< "You entered " + number << endl;
+    cerr<< "This is an example of an error message";
+}
+
