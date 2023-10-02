68c2c43c60d0318b3199108ec52760ad65d46f7a
Collins Muema <115388842+CMuema@users.noreply.github.com>
2023-10-02 08:51:43 +0300

    Create AssignmentDBT1102.cpp

---
+++ AssignmentDBT1102.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+int main() {
+    std::cout<< "Welcome to the wonderful world of C++!" <<std::endl;
+    std::cout<< "This is my first C++ output statement." <<std::endl;
+    std::cout<< "Soon, I will learn how to input data into my program." <<std::endl;
+
+    return 0;
+}
