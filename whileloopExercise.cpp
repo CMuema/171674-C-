983d9fbc706be1c0c70035746eb2fb5b729f3adf
Collins Muema <115388842+CMuema@users.noreply.github.com>
2023-11-22 11:06:49 +0300

    Create whileloopExercise.cpp

---
+++ whileloopExercise.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+    cout<<"A program to compute the sum of numbers between 20 and 25";
+    int num=20,sum=0;
+    while(num<=25) {
+        sum+=num;
+        num++;
+        }
+        cout<<"\nThe sum is "<<sum;
+        }
